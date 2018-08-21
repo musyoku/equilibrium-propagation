6698ae91dcc2cafe10a22799290fccef2d20fef1
musyoku <musyoku@users.noreply.github.com>
2018-08-22 00:00:47 +0900

    Fix some bugs

--- model.py
+++ model.py
@@ -5,47 +5,71 @@ from chainer.initializers import GlorotNormal
 
 
 class Model(chainer.Chain):
-    def __init__(self, ndim_hidden=512, num_hidden_layers=1):
+    def __init__(self, ndim_hidden_units=[512]):
         super().__init__()
-        self.ndim_hidden = ndim_hidden
-        self.num_hidden_layers = num_hidden_layers
+        assert len(ndim_hidden_units) > 0
+        self.ndim_hidden_units = ndim_hidden_units
 
-        forward_weights = chainer.ChainList()
-        backward_weights = chainer.ChainList()
+        forward_links = chainer.ChainList()
+        backward_links = chainer.ChainList()
 
-        with forward_weights.init_scope():
-            forward_weights.append(
-                L.Linear(784, ndim_hidden, initialW=GlorotNormal(1.0)))
-            for _ in range(num_hidden_layers):
-                forward_weights.append(
+        with forward_links.init_scope():
+            ndim_units = [784] + ndim_hidden_units + [10]
+            for in_units, out_units in zip(ndim_units[:-1], ndim_units[1:]):
+                forward_links.append(
                     L.Linear(
-                        ndim_hidden, ndim_hidden, initialW=GlorotNormal(1.0)))
-            forward_weights.append(
-                L.Linear(ndim_hidden, 10, initialW=GlorotNormal(1.0)))
+                        in_units,
+                        out_units,
+                        nobias=True,
+                        initialW=GlorotNormal(1.0)))
 
-        with backward_weights.init_scope():
-            backward_weights.append(
-                L.Linear(10, ndim_hidden, initialW=GlorotNormal(1.0)))
-            for _ in range(num_hidden_layers):
-                backward_weights.append(
+        with backward_links.init_scope():
+            ndim_units = ndim_hidden_units + [10]
+            for out_units, in_units in zip(ndim_units[:-1], ndim_units[1:]):
+                backward_links.append(
                     L.Linear(
-                        ndim_hidden, ndim_hidden, initialW=GlorotNormal(1.0)))
+                        in_units,
+                        out_units,
+                        nobias=True,
+                        initialW=GlorotNormal(1.0)))
 
         with self.init_scope():
-            self.forward_weights = forward_weights
-            self.backward_weights = backward_weights
+            self.forward_links = forward_links
+            self.backward_links = backward_links
+
+    def forward_backward_weight_pairs(self):
+        # e.g.
+        # network: x -> s1 -> s2 -> s3 -> y
+        # W_ij: j to i
+        # forward_weights = [W_10, W_21, W_32]
+        # backward_weights = [W_12, W_23]
+
+        forward_weights = []
+        backward_weights = []
+
+        for link in self.forward_links.children():
+            weight = link.W.data
+            forward_weights.append(weight)
+
+        for link in self.backward_links.children():
+            weight = link.W.data
+            backward_weights.append(weight)
+        backward_weights.append(None)
+
+        forward_backward_weight_pairs = []
+        for Wf, Wb in zip(forward_weights, backward_weights):
+            forward_backward_weight_pairs.append((Wf, Wb))
+
+        # [(W_10, W_12), (W_21, W_23), (W_32, None)]
+        return forward_backward_weight_pairs
 
     def generate_initial_states(self, batchsize):
         xp = self.xp
 
-        # input
-        states = [xp.random.uniform(0, 1, size=(batchsize, 784))]
+        states = []
 
-        # hidden
-        for _ in range(self.num_hidden_layers):
-            states.append(
-                xp.random.uniform(0, 1, size=(batchsize, self.ndim_hidden)))
-        # output
-        states.append(xp.random.uniform(0, 1, size=(batchsize, 10)))
+        ndim_units = self.ndim_hidden_units + [10]
+        for ndim in ndim_units:
+            states.append(xp.random.uniform(0, 1, size=(batchsize, ndim)))
 
         return states
--- train.py
+++ train.py
@@ -7,6 +7,14 @@ from chainer.backends import cuda
 from model import Model
 
 
+def rho(state):
+    if isinstance(state, cp.ndarray):
+        return cp.clip(state, 0, 1)
+    if isinstance(state, np.ndarray):
+        return np.clip(state, 0, 1)
+    raise NotImplementedError()
+
+
 def main():
     # gpu/cpu
     xp = np
@@ -16,8 +24,7 @@ def main():
         xp = cp
 
     # model
-    model = Model(
-        ndim_hidden=args.ndim_hidden, num_hidden_layers=args.num_hidden_layers)
+    model = Model(ndim_hidden_units=[123, 345, 567])
     if using_gpu:
         model.to_gpu()
 
@@ -26,13 +33,57 @@ def main():
 
     # training loop
     states = model.generate_initial_states(args.batchsize)
-    print(states)
+    if len(states) == 1:
+        neighbor_state_pairs = [(x, states[0], None)]
+    else:
+        neighbor_state_pairs = []
+        for layer_index, state in enumerate(states):
+            if layer_index == 0:
+                # input layer
+                top_state = states[layer_index + 1]
+                bottom_state = x
+                neighbor_state_pairs.append((bottom_state, state, top_state))
+                continue
+            if layer_index == len(states) - 1:
+                # output layer
+                bottom_state = states[layer_index - 1]
+                neighbor_state_pairs.append((bottom_state, state, None))
+                continue
+            # hidden layer
+            top_state = states[layer_index + 1]
+            bottom_state = states[layer_index - 1]
+            neighbor_state_pairs.append((bottom_state, state, top_state))
+
+    # compute vector field
+    mu_array = []
+    weight_pairs = model.forward_backward_weight_pairs()
+
+    # print("states")
+    # for state in states:
+    #     print(state.shape)
+
+    # print("weight_pairs")
+    # for (Wf, Wb) in weight_pairs:
+    #     if Wb is None:
+    #         print(Wf.shape)
+    #     else:
+    #         print(Wf.shape, Wb.shape)
+
+    assert len(states) == len(weight_pairs)
+    for (bottom_state, state, top_state), (Wf, Wb) in zip(
+            neighbor_state_pairs, weight_pairs):
+        if Wb is None:
+            assert top_state is None
+            mu = rho(bottom_state).dot(Wf.T) - state
+            mu_array.append(mu)
+            continue
+        mu = rho(bottom_state).dot(Wf.T) + rho(top_state).dot(Wb.T) - state
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--gpu-device", "-gpu", type=int, default=0)
-    parser.add_argument("--batchsize", "-bs", type=int, default=512)
+    parser.add_argument("--batchsize", "-bs", type=int, default=20)
     parser.add_argument("--ndim-hidden", "-ndim", type=int, default=512)
     parser.add_argument("--num-hidden-layers", "-layers", type=int, default=1)
     args = parser.parse_args()
