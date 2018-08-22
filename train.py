1e35cf121b5557dd65c7989b8dcdf0937c0c61f4
musyoku <musyoku@users.noreply.github.com>
2018-08-22 17:29:42 +0900

    seem to have succeeded in training the model

--- train.py
+++ train.py
@@ -25,6 +25,30 @@ def clip(state):
     raise NotImplementedError()
 
 
+def make_neighbor_state_pairs(states, x):
+    if len(states) == 1:
+        return [(x, states[0], None)]
+
+    neighbor_state_pairs = []
+    for layer_index, state in enumerate(states):
+        if layer_index == 0:
+            # input layer
+            top_state = states[layer_index + 1]
+            bottom_state = x
+            neighbor_state_pairs.append((bottom_state, state, top_state))
+            continue
+        if layer_index == len(states) - 1:
+            # output layer
+            bottom_state = states[layer_index - 1]
+            neighbor_state_pairs.append((bottom_state, state, None))
+            continue
+        # hidden layer
+        top_state = states[layer_index + 1]
+        bottom_state = states[layer_index - 1]
+        neighbor_state_pairs.append((bottom_state, state, top_state))
+    return neighbor_state_pairs
+
+
 def main():
     # gpu/cpu
     xp = np
@@ -34,7 +58,7 @@ def main():
         xp = cp
 
     # model
-    model = Model(ndim_hidden_units=[123, 345, 567])
+    model = Model(ndim_hidden_units=[555, 444, 333])
     if using_gpu:
         model.to_gpu()
 
@@ -45,27 +69,7 @@ def main():
         y = cuda.to_gpu(y)
 
     states = model.generate_initial_states(args.batchsize)
-    if len(states) == 1:
-        neighbor_state_pairs = [(x, states[0], None)]
-    else:
-        neighbor_state_pairs = []
-        for layer_index, state in enumerate(states):
-            if layer_index == 0:
-                # input layer
-                top_state = states[layer_index + 1]
-                bottom_state = x
-                neighbor_state_pairs.append((bottom_state, state, top_state))
-                continue
-            if layer_index == len(states) - 1:
-                # output layer
-                bottom_state = states[layer_index - 1]
-                neighbor_state_pairs.append((bottom_state, state, None))
-                continue
-            # hidden layer
-            top_state = states[layer_index + 1]
-            bottom_state = states[layer_index - 1]
-            neighbor_state_pairs.append((bottom_state, state, top_state))
-
+    neighbor_state_pairs = make_neighbor_state_pairs(states, x)
     weight_pairs = model.forward_backward_weight_pairs()
 
     # training loop
@@ -105,7 +109,8 @@ def main():
             # compute gradient of the loss function with respect to x
             output_state = states[-1]
             variable = chainer.Variable(output_state)
-            loss = cf.mean_squared_error(variable, y)
+            loss = cf.sum(0.5 * (y - variable)**2)
+            variable.cleargrad()
             loss.backward()
             grad_objective = variable.grad
 
@@ -132,11 +137,12 @@ def main():
                 new_state = clip(state + eps * mu)
                 state[...] = new_state
 
-        output_state = states[-1]
-        loss = xp.sum((output_state - y)**2)
-        print(output_state)
-        print(y)
-        print(loss)
+        # output_state = states[-1]
+        # loss = xp.sum((output_state - y)**2)
+        # print(grad_objective)
+        # print(output_state)
+        # print(y)
+        # print(loss)
 
         # store states obtained in the first phase
         states_in_second_phase = []
@@ -144,57 +150,70 @@ def main():
             states_in_second_phase.append(xp.copy(state))
 
         # update weights
-        # mu_array = []
-        # assert len(states) == len(weight_pairs)
-        # for (bottom_state, state,
-        #      top_state), (Wf, Wb), state_first, state_second in zip(
-        #          neighbor_state_pairs, weight_pairs, states_in_first_phase,
-        #          states_in_second_phase):
-        #     vWf = chainer.Variable(Wf)
-        #     mu = cf.sum(
-        #         cf.connection.linear.linear(rho(bottom_state), vWf) *
-        #         (state_second - state_first))
-        #     mu.backward()
-        #     Wf[...] -= 0.01 * vWf.grad
+        mu_array = []
+        neighbor_state_pairs_in_first_phase = make_neighbor_state_pairs(
+            states_in_first_phase, x)
+        assert len(states) == len(weight_pairs)
+        for (bottom_state, state,
+             top_state), (Wf, Wb), state_first, state_second in zip(
+                 neighbor_state_pairs_in_first_phase, weight_pairs,
+                 states_in_first_phase, states_in_second_phase):
+            vWf = chainer.Variable(Wf.T)
 
-        #     if Wb is None:
-        #         continue
+            # output layer
+            if Wb is None:
+                assert top_state is None
+                mu = cf.connection.linear.linear(rho(bottom_state),
+                                                 vWf) - state
+                mu = cf.sum(mu * (state_second - state_first))
+                vWf.cleargrad()
+                mu.backward()
+                Wf[...] -= 0.01 * vWf.grad.T
+                continue
 
-        #     vWb = chainer.Variable(Wb)
-        #     mu = cf.sum(
-        #         cf.connection.linear.linear(rho(top_state), vWb) *
-        #         (state_second - state_first))
-        #     mu.backward()
-        #     Wb[...] -= 0.01 * vWb.grad
+            vWb = chainer.Variable(Wb.T)
+            mu = cf.connection.linear.linear(
+                rho(bottom_state), vWf) + cf.connection.linear.linear(
+                    rho(top_state), vWb) - state
+            mu = cf.sum(mu * (state_second - state_first))
+            vWb.cleargrad()
+            mu.backward()
+            Wb[...] -= 0.01 * vWb.grad.T
 
         # evaluate
-        # for iteration in range(200):
-        #     # compute vector field
-        #     mu_array = []
-        #     assert len(states) == len(weight_pairs)
-        #     for (bottom_state, state, top_state), (Wf, Wb) in zip(
-        #             neighbor_state_pairs, weight_pairs):
-        #         # output layer
-        #         if Wb is None:
-        #             assert top_state is None
-        #             mu = rho(bottom_state).dot(Wf) - state
-        #             mu_array.append(mu)
-        #             continue
+        for iteration in range(200):
+            # compute vector field
+            mu_array = []
+            assert len(states) == len(weight_pairs)
+            for (bottom_state, state, top_state), (Wf, Wb) in zip(
+                    neighbor_state_pairs, weight_pairs):
+                # output layer
+                if Wb is None:
+                    assert top_state is None
+                    mu = rho(bottom_state).dot(Wf) - state
+                    mu_array.append(mu)
+                    continue
 
-        #         # other layers
-        #         mu = rho(bottom_state).dot(Wf) + rho(top_state).dot(
-        #             Wb) - state
-        #         mu_array.append(mu)
+                # other layers
+                mu = rho(bottom_state).dot(Wf) + rho(top_state).dot(Wb) - state
+                mu_array.append(mu)
 
-        #     # update states
-        #     eps = 0.001
-        #     for state, mu in zip(states, mu_array):
-        #         new_state = rho(state - eps * mu)
-        #         state[...] = new_state
+            # update states
+            eps = 0.001
+            for state, mu in zip(states, mu_array):
+                new_state = rho(state - eps * mu)
+                state[...] = new_state
 
-        # output_state = states[-1]
-        # loss = xp.sum((output_state - y)**2)
-        # print(loss)
+        output_state = states[-1]
+        loss = xp.sum((output_state - y)**2)
+        print("output")
+        print(output_state)
+        print("target")
+        print(y)
+        print("output　distribution")
+        print(output_state / xp.sum(output_state, axis=1, keepdims=True))
+        print("target distribution")
+        print(y / xp.sum(y, axis=1, keepdims=True))
 
 
 if __name__ == "__main__":
